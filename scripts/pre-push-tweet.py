da9d2fed676a8dc61a58af484754b0e4f78b0a36
Brian Wyant <wyantb@gmail.com>
2014-05-31 12:27:56 -0400

    Skip pre-push-tweet when pushing to new branch

--- scripts/pre-push-tweet.py
+++ scripts/pre-push-tweet.py
@@ -4,16 +4,28 @@ import sys
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
 
-get_branch_command = ["git", "symbolic-ref", "HEAD"]
+remote = sys.argv[0]
+if (remote != "origin"):
+    print "Not pushing to origin, skipping pre-push-tweet.py"
+    sys.exit(0)
+
+get_branch_command = ["git", "symbolic-ref", "--quiet", "HEAD"]
 is_on_branch = subprocess.call(get_branch_command)
 if (is_on_branch != 0):
     print "Detached head; skipping pre-push-tweet.py"
     sys.exit(0)
 
 branch = subprocess.check_output(get_branch_command).replace("refs/heads/", "").strip()
-print "Pushing to branch: " + branch
 
-push_spec = "origin/" + branch + ".." + branch
+remote_ref = "refs/remotes/origin/" + branch
+check_for_remote_ref = subprocess.call(["git", "show-ref", "--verify", "--quiet", remote_ref])
+if (check_for_remote_ref != 0):
+    print "Pushing to new branch; skipping pre-push-tweet.py"
+    sys.exit(0)
+
+print "Pushing to existing branch, will copy to clipboard: " + branch
+
+push_spec = remote_ref + ".." + branch
 messages = subprocess.check_output(["git", "log", push_spec, "--pretty=format:%s"])
 line_subjects = messages.split("\n")
 joined_subjects = ["'" + msg + "'" for msg in line_subjects]
