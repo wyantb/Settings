e6bf3bf07e776f7d0c2bd6938301a09c23f9a5dc
Brian Wyant <wyantb@gmail.com>
2014-10-28 16:22:36 -0400

    scripts: add script for checking what's running on a given port

---
+++ scripts/portcheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+port=$1
+procinfo=$(netstat --numeric-ports -nlp 2> /dev/null | grep ^tcp | grep -w ${port} | tail -n 1 | awk '{print $7}')
+
+case "${procinfo}" in
+"")
+echo "No process listening on port ${port}"
+;;
+"-")
+echo "Process is running on ${port}, but current user does not have rights to see process information."
+;;
+*)
+echo "${procinfo} is running on port ${port}"
+ps -uwep ${procinfo%/*}
+;;
+esac
